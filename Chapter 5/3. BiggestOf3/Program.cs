ec05e7b1807aead61da270d1262e8fa09688f5a9
sarf01k <isaacksarfo1@gmail.com>
2024-10-07 21:12:06 +0000

    Chapter 5

---
+++ Chapter 5/3. BiggestOf3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BiggestOf3
+{
+    class Prgoram
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter 3 integers:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int biggest;
+
+            if (num2 > num1 || num3 > num1)
+            {
+                if (num2 > num3)
+                {
+                    biggest = num2;
+                }
+                else
+                {
+                    biggest = num3;
+                }
+            }
+            else
+            {
+                biggest = num1;
+            }
+
+            Console.WriteLine("Biggest: {0}", biggest);
+        }
+    }
+}
---
+++ Chapter 5/6. QuadraticEquation/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a, b & c:");
+            float a = float.Parse(Console.ReadLine());
+            float b = float.Parse(Console.ReadLine());
+            float c = float.Parse(Console.ReadLine());
+
+            float d = (float)(Math.Pow(b, 2) - (4 * a * c));
+
+            if (d == 0)
+            {
+                float x = -b / (2 * a);
+
+                Console.WriteLine("One real root:", x);
+            }
+            else if (d > 0)
+            {
+                float x1 = (float)(-b + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c)) / (2 * a));
+                float x2 = (float)(-b - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c)) / (2 * a));
+
+                Console.WriteLine("Two distinct real roots: {0}, {1}", x1, x2);
+            }
+            else
+            {
+                Console.WriteLine("No real roots.");
+            }
+        }
+    }
+}
