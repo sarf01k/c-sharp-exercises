99a6d667640546371189cdd9ba755dce939ea05f
sarf01k <isaacksarfo1@gmail.com>
2024-10-12 22:09:21 +0000

    Chapter 5 done

--- Chapter 5/11. ConvertToWords/Program.cs
+++ Chapter 5/11. ConvertToWords/Program.cs
@@ -2,43 +2,55 @@
 {
     class Program
     {
-        private static string ToWords(int number)
+        private static string Digit(int number)
         {
             switch (number)
             {
-                case 0:
-                    return "zero";
-                    break;
                 case 1:
                     return "one";
-                    break;
                 case 2:
                     return "two";
-                    break;
                 case 3:
                     return "three";
-                    break;
                 case 4:
                     return "four";
-                    break;
                 case 5:
                     return "five";
-                    break;
                 case 6:
                     return "six";
-                    break;
                 case 7:
                     return "seven";
-                    break;
                 case 8:
                     return "eight";
-                    break;
                 case 9:
                     return "nine";
-                    break;
                 default:
-                    return "number must be between [0...999]";
-                    break;
+                    return "Number must be between [0...999]";
+            }
+        }
+
+        private static string DDigit(int number)
+        {
+            switch (number)
+            {
+                case 2:
+                    return "twenty";
+                case 3:
+                    return "thirty";
+                case 4:
+                    return "forty";
+                case 5:
+                    return "fifty";
+                case 6:
+                    return "sixty";
+                case 7:
+                    return "seventy";
+                case 8:
+                    return "eighty";
+                case 9:
+                    return "ninety";
+                default:
+                    return "Number must be between [0...999]";
             }
         }
 
@@ -46,116 +58,94 @@
         {
             Console.Write("Enter a number [0...999]: ");
             int number = int.Parse(Console.ReadLine());
-            string words;
+            string words = "";
 
-            if (number <= 9)
+            if (number >= 0 && number <= 999)
             {
-                words = ToWords(number);
+                if (number == 0)
+                    words = "Zero";
+
+                if (number >= 100)
+                {
+                    words = Digit(number / 100);
+                    words += " hundred";
+
+                    if ((number % 100) / 10 >= 1 && (number % 100) / 10 <= 9)
+                    {
+                        words += " and " + DDigit((number % 100) / 10);
+
+                        if ((number % 10) >= 1 && (number % 10) <= 9)
+                        {
+                            words += " " + Digit((number % 10));
+                        }
+                    }
+
+                    if ((number % 100) / 10 == 0)
+                    {
+                        words += " and " + Digit((number % 10));
+                    }
+                }
+
+                if (number >= 20)
+                {
+                    words += DDigit(number / 10);
+
+                    if (number % 10 > 0)
+                    {
+                        words += " " + Digit(number % 10);
+                    }
+                }
+
+                if (number < 20)
+                {
+                    if (number / 10 > 0)
+                    {
+                        switch (number % 10)
+                        {
+                            case 0:
+                                words += "eleven";
+                                break;
+                            case 1:
+                                words += "eleven";
+                                break;
+                            case 2:
+                                words += "twelve";
+                                break;
+                            case 3:
+                                words += "thirteen";
+                                break;
+                            case 4:
+                                words += "fourteen";
+                                break;
+                            case 5:
+                                words += "fifteen";
+                                break;
+                            case 6:
+                                words += "sixteen";
+                                break;
+                            case 7:
+                                words += "seventeen";
+                                break;
+                            case 8:
+                                words += "eighteen";
+                                break;
+                            case 9:
+                                words += "nineteen";
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        words = Digit(number);
+                    }
+                }
             }
             else
             {
-                if (number >= 10)
-                {
-                    switch (number / 10)
-                    {
-                        case 1:
-                            words = "Ten";
-                            break;
-                        case 2:
-                            words = "Twenty";
-                            break;
-                        case 3:
-                            words = "Thirty";
-                            break;
-                        case 4:
-                            words = "Forty";
-                            break;
-                        case 5:
-                            words = "Fifty";
-                            break;
-                        case 6:
-                            words = "Sixty";
-                            break;
-                        case 7:
-                            words = "Seventy";
-                            break;
-                        case 8:
-                            words = "Eighty";
-                            break;
-                        case 9:
-                            words = "Ninety";
-                            break;
-                        default:
-                            words = "Number must be between [0...999]";
-                            break;
-                    }
-
-                    switch (number % 10)
-                    {
-                        case 1:
-                            words += " one";
-                            break;
-                        case 3 :
-                            words = "Thirty";
-                            break;
-                        case 4:
-                            words = "Forty";
-                            break;
-                        case 5:
-                            words = "Fifty";
-                            break;
-                        case 6:
-                            words = "Sixty";
-                            break;
-                        case 7:
-                            words = "Seventy";
-                            break;
-                        case 8:
-                            words = "Eighty";
-                            break;
-                        case 9:
-                            words = "Ninety";
-                            break;
-                        default:
-                            words = "Number must be between [0...999]";
-                            break;
-                    }
-                }
-
-                switch (number / 100)
-                {
-                    case 1:
-                        words = "Hundred";
-                        break;
-                    case 2:
-                        words = "Two hundred";
-                        break;
-                    case 3:
-                        words = "Three hundred";
-                        break;
-                    case 4:
-                        words = "Four hundred";
-                        break;
-                    case 5:
-                        words = "Five hundred";
-                        break;
-                    case 6:
-                        words = "Six hundred";
-                        break;
-                    case 7:
-                        words = "Seven hundred";
-                        break;
-                    case 8:
-                        words = "Eight hundred";
-                        break;
-                    case 9:
-                        words = "Nine hundred";
-                        break;
-                    default:
-                        words = "Number must be between [0...999]";
-                        break;
-                }
+                words = "Number must be between [0...999]";
             }
+
+            Console.WriteLine(words.Substring(0, 1).ToUpper() + words.Substring(1));
         }
     }
 }
