dc3d9631a9bd79f724d00ee3ea540cf778beb1e7
sarf01k <isaacksarfo1@gmail.com>
2024-10-21 21:20:25 +0000

    Ex. 16

---
+++ Chapter 6/16. Random1ToN/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Random1ToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[2];
+            numbers[0] = 1;
+
+            Console.Write("Enter a number > 1: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // for (int i = 2; i <= 2; i++)
+            // {
+                numbers.Append(2);
+                // Console.WriteLine(numbers[i]);
+            // }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
