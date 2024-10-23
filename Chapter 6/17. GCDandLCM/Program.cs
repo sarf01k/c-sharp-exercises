0329376d30984381208f62e3c0b8507bf931beb4
sarf01k <isaacksarfo1@gmail.com>
2024-10-23 22:46:00 +0000

    Chapter 6 update

--- Chapter 6/17. GCDandLCM/Program.cs
+++ Chapter 6/17. GCDandLCM/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter two numbers:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int product = Math.Abs(num1 * num2);
+
+            do
+            {
+                if (num1 > num2)
+                    num1 -= num2;
+                else
+                    num2 -= num1;
+            } while (num1 != num2);
+
+            Console.WriteLine("GCD is {0}", num1);
+            Console.WriteLine("LCM is {0}", product/num1);
         }
     }
 }
---
+++ Chapter 6/18. Spiral Matrix/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace SpiralMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            byte n = byte.Parse(Console.ReadLine());
+
+            byte[,] matrix = new byte[n, n];
+        }
+
+        private static void FillMatrix(byte[,] matrix, byte n)
+        {
+            // starting position
+            byte positionY = 0;
+            byte positionX = 0;
+
+            byte direction = 0;
+            byte stepCount = 3;
+            byte stepPosition = 0;
+
+            for (byte i = 1; i <= n; i++)
+            {
+                matrix[positionY, positionX] = i;
+
+                if (stepPosition < stepCount)
+                {
+                    stepPosition++;
+                }
+                else
+                {
+                    stepPosition = 1;
+                }
+
+                switch (direction)
+                {
+                    case 0:
+                        positionX++;
+                        break;
+                    case 1:
+                        positionY++;
+                        break;
+                    case 2:
+                        positionX--;
+                        break;
+                    case 3:
+                        positionY--;
+                        break;
+                }
+            }
+        }
+
+        private static void PrintMatrix(byte[,] matrix, byte n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0,3} ", matrix[i, j]);
+                }
+            }
+        }
+    }
+}
