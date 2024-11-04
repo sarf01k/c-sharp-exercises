c56d90e3345f65e19bbb26722c792cbfd373a21e
sarf01k <isaacksarfo1@gmail.com>
2024-11-04 19:58:02 +0000

    C7 E8

--- Chapter 7/6. IncreasingSequence/Program.cs
+++ Chapter 7/6. IncreasingSequence/Program.cs
@@ -6,58 +6,37 @@
         {
             byte[] elements = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 4 };
 
-            byte[] maxSequence = new byte[elements.Length];
-            var maxSequenceList = new List<byte>();
+            byte[] sequenceLengths = new byte[elements.Length];
 
             byte start = elements[0];
             byte len = 1;
             byte bestLen = len;
 
+            // for (int i = 0; i < elements.Length; i++)
+            // {
+            //     len = 1;
+            //     for (int j = i + 1; j < elements.Length; j++)
+            //     {
+            //         if (elements[j] > elements[i])
+            //         {
+            //             len++;
+            //         }
+            //     }
+            //     sequenceLengths[i] = len;
+            //     Console.WriteLine(sequenceLengths[i]);
+            // }
+
             for (int i = 0; i < elements.Length; i++)
             {
-                start = elements[i];
-                maxSequenceList = [ start ];
-                if (i != elements.Length - 1)
+                for (int j = 0; j < elements.Length                                                                                                                                      ; j++)
                 {
-                    for (int j = i + 1; j < elements.Length; j++)
-                    {
-                        if (elements[j] > start)
-                            maxSequenceList.Add(elements[j]);
-                    }                                                            
-                    len = (byte)maxSequenceList.Count;
-                }
-                
-                if (len > bestLen)
-                {
-                    bestLen = len;
-                    maxSequence = maxSequenceList.Distinct().ToArray();
+                    
                 }
             }
 
-            byte biggest = maxSequence.Max();
-            Array.Resize(ref maxSequence, Array.IndexOf(maxSequence, biggest) + 1);
-
-            //
-            for (int k = 0; k < maxSequence.Length; k++)
-            {
-                Console.Write(maxSequence[k]);
-            }
-            Console.WriteLine();
-            //
-
-            maxSequenceList = maxSequence.ToList();
-            for (int i = 1; i < maxSequenceList.Count - 1; i++)
-            {
-                if (maxSequenceList[i] > maxSequenceList[i+1] && maxSequenceList[i] > maxSequenceList[i-1] || maxSequenceList[i] < maxSequenceList[i+1] && maxSequenceList[i] < maxSequenceList[i-1])
-                {
-                    maxSequenceList.Remove(maxSequenceList[i]);
-                }
-            }
-
-            for (int k = 0; k < maxSequenceList.Count; k++)
-            {
-                Console.Write(maxSequenceList[k]);
-            }
+            byte longest = sequenceLengths.ToList().Max();
+            Array.IndexOf(sequenceLengths, longest);
+            Console.WriteLine(longest);
         }
     }
 }
---
+++ Chapter 7/8. ArraySort/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ArraySort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] elements = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 4 };
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.Write("{0} ", elements[i]);
+            }
+
+            byte temp, smallest;
+            for (int i = 0; i < elements.Length - 1; i++)
+            {
+                smallest = (byte)i;
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    if (elements[j] < elements[smallest])
+                        smallest = (byte)j;
+                }
+
+                temp = elements[smallest];
+                elements[smallest] = elements[i];
+                elements[i] = temp;
+            }
+
+            Console.WriteLine("\nSorted:");
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.Write("{0} ", elements[i]);
+            }
+        }
+    }
+}
