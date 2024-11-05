01a5b9adef7d2ff3f3bc2e295f07dbaf5fd7ab4f
sarf01k <isaacksarfo1@gmail.com>
2024-11-05 16:35:13 +0000

    C7 E10

---
+++ Chapter 7/10. MostFrequent/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MostFrequent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] elements = { 9, 6, 2, 7, 4, 7, 6, 5, 8, 4, 4, 4, 4, 4, 4 };
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.Write("{0} ", elements[i]);
+            }
+
+            byte mostFreq = byte.MinValue;
+            byte len = 1;
+            byte bestLen = len;
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                len = 1;
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    if (elements[j] == elements[i])
+                        len++;
+
+                    if (len > bestLen)
+                    {
+                        bestLen = len;
+                        mostFreq = elements[i];
+                    }   
+                }
+            }
+
+            Console.WriteLine("\nMost frequent: {0}({1} times)", mostFreq, bestLen);
+        }
+    }
+}
---
+++ Chapter 7/11. CertainSum/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace CertainSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] numbers = { 4, 3, 1, 4, 2, 5, 8 };
+
+            Console.Write("Numbers: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("{0} ", numbers[i]);
+            }
+
+            Console.Write("\n\nEnter a number: ");
+            byte target;
+            byte.TryParse(Console.ReadLine(), out target);
+
+            byte sum;
+            byte len = 1;
+            byte startIndx;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = numbers[i];
+                startIndx = (byte)i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (sum == target)
+                    {
+                        Console.Write("{0} -> ", target);
+                        for (int k = startIndx; k < startIndx + len; k++)
+                        {
+                            Console.Write("{0}", numbers[k]);
+                            break;
+                        }
+                    }
+                    else if (sum > target)
+                    {
+                        Console.WriteLine("\nNo sequence.");
+                        break;
+                    }
+                    else
+                    {
+                        sum += numbers[j];
+                        len++;
+                    }
+                }
+            }
+        }
+    }
+}
