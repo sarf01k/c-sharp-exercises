c075dc626123ea8c1c4a77de9c89d6a920ca8e22
sarf01k <isaacksarfo1@gmail.com>
2025-01-28 00:06:13 +0000

    update

---
+++ Chapter 7/16. BinarySearch/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = { 2, 3, 4, 10, 40 };
+
+BinarySearch(arr, 10);
+
+static void BinarySearch(int[] arr, int target)
+{
+    Array.Sort(arr);
+    int low = 0;
+    int high = arr.Length - 1;
+
+    while (low <= high)
+    {
+        int mid = low + ((high - low) / 2);
+
+        if (arr[mid] < target)
+            low = mid + 1;
+        else if (arr[mid] > target)
+            high = mid - 1;
+        else if (arr[mid] == target)
+        {
+            Console.WriteLine($"Element is present at index {Array.IndexOf(arr, arr[mid])}");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("Element is not present in array");
+            return;
+        }
+    }
+}
---
+++ Chapter 7/17. MergeSort/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] arr = { 2, 3, 4, 10, 40 };
+
+MergeSort(arr);
+
+static void MergeSort(int[] arr)
+{
+
+}
